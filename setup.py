6e9a3f904e572525f34e0fd132b6d340bd6b560b
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2024-07-06 07:52:45 +0900

    update package dependencies
    - add "python-box" library

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["pyyaml", "argparse", "python-docx>=0.8.7"],  # Optional
+    install_requires=["pyyaml", "python-box", "argparse", "python-docx>=0.8.7"],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
